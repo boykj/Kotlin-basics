768c7219b1683fc51e9ca85f659ce886443e6eb5
boykj <jboyk918@gmail.com>
2022-09-20 11:12:21 -0700

    walkthrough.kt: add additional printMessage func with prefix notation

--- walkthrough.kt
+++ walkthrough.kt
@@ -4,6 +4,10 @@ fun printMessage(message: String): Unit {
     println(message)    
 }
 
+fun printMessageWithPrefix(message: String, prefix: String = "info") {
+    println("[$prefix] $message")
+}
+
 fun main() {
     println("Hello, world")
 }
