7f6bd3dba179fa54dd683262efe7893e9cce1c69
boykj <jboyk918@gmail.com>
2022-09-20 11:33:38 -0700

    infix-functions.kt: Add additional infix function for string printing: Alter line spacing for fun main()

--- infix-functions.kt
+++ infix-functions.kt
@@ -1,6 +1,10 @@
 // Member functions and extensions with a single parameter can be "infix functions"
 
 fun main() {
+
     infix fun Int.times(str: String) = str.repeat(this)
     println(2 times "Bye ")
+
+    val pair = "Ferrari" to "Katrina"
+    println(pair)
 }
