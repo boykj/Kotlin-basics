5eadeb78178ce3e8a918b05f090f815e5bd6d42d
boykj <jboyk918@gmail.com>
2022-09-20 09:53:57 -0700

    basic-syntax.kt: Add basic integer function

--- basic-syntax.kt
+++ basic-syntax.kt
@@ -9,4 +9,15 @@ fun main() {
 
     // Print to stdout
     print("world")
+
+    println()
+
+    // Basic function with two integer parameters, with a integer return
+    fun sum(a: Int, b: Int): Int {
+    	return a + b
+    }
+
+    val result = sum(1, 2)
+    println(result)
+
 }
