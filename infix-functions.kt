11583271418360175fa40252fc3a000ce76b8f09
boykj <jboyk918@gmail.com>
2022-09-20 11:31:29 -0700

    infix-functions.kt: Initial build commit: Function will print Bye x2, based on the integer supplied with times

---
+++ infix-functions.kt
@@ -0,0 +1,6 @@
+// Member functions and extensions with a single parameter can be "infix functions"
+
+fun main() {
+    infix fun Int.times(str: String) = str.repeat(this)
+    println(2 times "Bye ")
+}
