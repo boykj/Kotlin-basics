36737bcddd24e383edc005b0d66a74ca76637e5d
boykj <jboyk918@gmail.com>
2022-09-20 11:02:34 -0700

    basic-syntax.kt: Add open Rectangle class - inheritable

--- basic-syntax.kt
+++ basic-syntax.kt
@@ -12,6 +12,7 @@ fun main() {
 
     println()
 
+    /****************** Functions ***********************
     // Basic function with two integer parameters, with a integer return
     fun sum(a: Int, b: Int): Int {
     	return a + b
@@ -37,7 +38,9 @@ fun main() {
 
     val newResult_2 = printSum(10, 11)
 
-    // Class instantiation
+    *****************************************************/
+
+    // Class definition
     class Rectangle(var height: Double, var length: Double) {
     	  var perimeter = (height + length) * 2
     }
@@ -45,5 +48,18 @@ fun main() {
     // Class call and print
     val rectangle = Rectangle(5.0, 2.0)
     println("Perimeter is ${rectangle.perimeter}")
+
+    // Inheritance can be denoted by a colon (:)
+    // Classes are final by default, to ensure interitance, mark the class as open
+
+    open class Shape
+
+    class OpenRectangle(var height: Double, var length: Double): Shape() {
+    	  var perimeter = (height + length) * 2
+    }
+
+    // Open rectangle instantiation and call
+    val openRectangle = OpenRectangle(10.0, 12.0)
+    println("Perimeter of inheritted rectangle is ${openRectangle.perimeter}")
 
 }
